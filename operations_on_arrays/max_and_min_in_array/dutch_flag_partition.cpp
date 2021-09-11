1b371e6170d6a7d6847f71ed271f7ca32707c30e
Pulkit Joshi <Pulkitthegamejoshi@gmail.com>
2021-09-11 11:49:23 +0530

    DNFP to sort an array of 3 colors without using any sorting algorithm.

--- operations_on_arrays/max_and_min_in_array/dutch_flag_partition.cpp
+++ operations_on_arrays/max_and_min_in_array/dutch_flag_partition.cpp
@@ -1,1 +1,40 @@
+/*
+DNFP can be used in the problem given.
+Problem: Given an array A[] consisting 0s, 1s and 2s. The task is to write a function that sorts the array, without using any sorting algorithms.
+Solution: By DNFP, O(n) time and O(1) space used.
+*/
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector <int> dnfp_sort(vector <int> a, int n) {
+    int low = 0, mid = 0, high = n - 1;
+    while(mid <= high) {
+        switch(a[mid]) {
+            case 0:
+                swap(a[low++], a[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(a[mid], a[high--]);
+                break;
+        }
+    }
+    return a;
+}
+
+int main() {
+    int size;
+    cin >> size;
+    vector <int> a(size);
+    for (int i = 0; i < size; i++) {
+        cin >> a[i];
+    }
+    a = dnfp_sort(a, size);
+    for (int i = 0; i < size; i++) {
+        cout << a[i] << " ";
+    }
+}
